1b275144a117115acfe6be79cf4689d19d8d9be0
Andreas Binder <andreasjosef.binder@gmail.com>
2023-04-29 20:26:53 +0200

    update app.py file

--- README.rst
+++ README.rst
@@ -33,17 +33,13 @@
 AI-Ideation-Assistant
 =====================
 
-
-    Add a short description here!
-
-
-A longer description of your project goes here...
-
+Run the app by using :code:`streamlit run src/ai_ideation_assistant/app.py `
 
 .. _pyscaffold-notes:
 
-Note
+Requirements
 ====
 
-This project has been set up using PyScaffold 4.4.1. For details and usage
-information on PyScaffold see https://pyscaffold.org/.
+* openAI
+* streamlit_chat
+* dotenv
--- src/ai_ideation_assistant/app.py
+++ src/ai_ideation_assistant/app.py
@@ -1,20 +1,122 @@
 import streamlit as st
-import streamlit_chat
-import openai
-import numpy as np
-import pandas as pd
-from PIL import Image
-
-# Has to be the first line
-#dosen't work yet
-# image = Image.open('C:\Users\ivivi\Desktop\Personal\tum.ai_makethon23\docs\resources\aai-logo.jpg')
-# st.image(image)
-# st.set_page_config(page_title="Title", page_icon=st.image(Image.open(r'docs/resources/aai-logo-removebg-preview.jpg'), caption='logo'))
-
-st.write("""
-# AppliedAI's AI expert is here to help you!
-""")
 from streamlit_chat import message
-message("Hello I'm your digital assistant created by AppliedAI. Together we can find the most suitable AI solution for your problem.")
-message("Tell me about your problem.")
-message("We are a manufacturer creating break systems that can be integrated in the car wheel. Lately the quality of our parts has dropped and we don't know the reason. Can you help us fix it?", is_user=True)
+import requests
+
+import os
+
+import openai
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+INITIAL_INSTRUCTOR_PROMPT = 'Consider the following scenario: you are helping a company  \
+                            identify beneficial AI use cases based on some domain-specific \
+                            inputs they provide you. You have to be the one to start the \
+                            conversation, the user will follow your instructions. \
+                            Start by greeting the customer. \
+                            Ask questions until you feel confident in giving a complete scenario \
+                            and use case descriptions. \
+                            Also, try to be as concise as possible and explore the problem step by step \
+                            ' 
+
+INITIAL_RESPONSE = 'Hello! Thank you for reaching out to us for help in identifying  \
+                    beneficial AI use cases. Can you tell me more about your company \
+                    and the industry you operate in? '
+
+INITIAL_MAX_TOKENS = 40
+
+
+def initialize_LM(cached_messages):
+
+    initial_instructor_message = set_message(INITIAL_INSTRUCTOR_PROMPT, role="system")
+    cached_messages.append(initial_instructor_message)
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        max_tokens = INITIAL_MAX_TOKENS,
+        messages=cached_messages
+    )
+    return cached_messages, response
+
+
+def query_LM(message, cached_messages):
+
+    cached_messages.append(message)
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        #max_tokens = INITIAL_MAX_TOKENS,
+        messages=cached_messages
+    )
+    return cached_messages, response
+
+def set_message(message, role):
+    return {"role": role, "content": message}
+
+def set_user_message(message):
+    return {"role": "user", "content": message}
+
+def get_text():
+    input_text = st.text_input("You: ","", key="input")
+    return input_text 
+
+def load_cache(cached_messages):
+    #cached_messages = []
+
+    for user_msg in st.session_state.past:
+        cached_messages.append(set_message(user_msg, "user"))
+
+    for assistance_msg in st.session_state.generated:
+        cached_messages.append(set_message(assistance_msg, "assistant"))
+        
+    return cached_messages
+
+def main():
+    st.set_page_config(
+        page_title="Streamlit Chat - Demo",
+        page_icon=":robot:"
+    )
+
+    st.header("Streamlit Chat - Demo")
+
+    cached_messages = []
+
+    cached_messages, response = initialize_LM(cached_messages)
+
+    # TODO add first instructor prompt
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = [response['choices'][0]['message']['content']]
+
+    if 'past' not in st.session_state:
+        st.session_state['past'] = ['Start of the Conversation']
+
+    user_input = get_text()
+
+    if user_input:
+
+        msg = set_message(user_input, role="user")
+        cached_messages = load_cache(cached_messages)
+
+        cached_messages, output = query_LM(msg, cached_messages)
+        output_text = output['choices'][0]['message']['content']
+
+        st.session_state.past.append(user_input)
+        #st.session_state.generated.append(output["generated_text"])
+        st.session_state.generated.append(output_text)
+
+    st.write("length: " + str(len(st.session_state['generated'])))
+
+
+    # Start
+    if st.session_state['generated']:
+        
+        for i in range(len(st.session_state['generated'])-1, -1, -1):
+            message(st.session_state["generated"][i], key=str(i))
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+            
+
+
+if __name__ == "__main__":
+    main()
