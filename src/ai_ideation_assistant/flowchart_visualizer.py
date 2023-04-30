7cb321029c7e0de216805e3b3c9dd19e7bdefd30
Ayushman_Choudhuri <ayushc205@gmail.com>
2023-04-30 05:32:25 +0200

    latex_to_pdf fixed

--- src/ai_ideation_assistant/flowchart_visualizer.py
+++ src/ai_ideation_assistant/flowchart_visualizer.py
@@ -1,22 +1,22 @@
 import openai
 import os
 from dotenv import load_dotenv
-from pylatex import Document
-import sys
+import subprocess
 
 load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-#openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_key = "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"
 
 def generate_flowchart(input_text):
     prompt = f"Represent this text in a latex flowchart with only the keypoints mentioned in the flowchart. \
                Use the tikz and xcolor package. draw a rectangular block around each block and the total diagram  \
-               Space the flowchart elements evenly . If there exists a feedback loop process such as refining, feedback , etc add the correct loop in the digram \
-               The close loop arrow needs to be the same thickness as the other arrow. \
-               Use only 90 degree bends on arrows\
+               Output needs to be only in one page\
                Only show the code.:  \n\n{input_text}"
+    
+    #Exprimental Prompts 
+    #Space the flowchart elements evenly . If there exists a feedback loop process such as refining, feedback , etc add the correct loop in the digram \
+    #The close loop arrow needs to be the same thickness as the other arrow. \
+    #Use only 90 degree bends on arrows\
 
     request = {"role": "system", "content": prompt}    
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", max_tokens = 520, messages=[request])
@@ -24,24 +24,17 @@ def generate_flowchart(input_text):
     return response['choices'][0]['message']['content']
         
 def latex_to_pdf(latex_file_path):
-    # Create a PyLaTeX document object
-    doc = Document()
-
-    # Load the LaTeX file into the document object
-    with open(latex_file_path, 'r') as f:
-        doc.append(f.read())
-
-    # Compile the document to PDF
+    # Compile the LaTeX file to a PDF file
     pdf_file_path = os.path.splitext(latex_file_path)[0] + '.pdf'
-    doc.generate_pdf(pdf_file_path)
-
+    subprocess.run(['pdflatex', '-interaction=nonstopmode', latex_file_path], check=True)
+    
     return pdf_file_path
 
 
 ########################################### test ##########################################3###
 
 #Import a test chat gpt output
-with open('gpt_output_sample_3.txt', 'r') as file:
+with open('./gpt-sample-outputs/gpt_output_sample_3.txt', 'r') as file:
     input_text = file.read()
 
 ## Generate a latex code from the gpt prompt
@@ -52,5 +45,6 @@ with open('output_latex.tex', 'w') as f:
     f.write(output)
 
 pdf_file_path = latex_to_pdf('output_latex.tex')
+
 
 #################################################################################################
